fad90e70cccd78cfed2c7f395c6834dbc4ea5725
Daan Meijer <daan@studioseptember.nl>
2019-02-16 19:54:17 +0100

    better page layout

--- web/app/themes/xrnl/index.php
+++ web/app/themes/xrnl/index.php
@@ -1,11 +1,25 @@
 <?PHP
 get_header();
 
+?>
+<div class="container">
+<?PHP
+
+?>
+
+    <div class="row">
+        <div class="col-12">
+    <?PHP
 while(have_posts()){
     the_post();
 
     ?><h1><?PHP the_title(); ?></h1><?PHP
     the_content();
 }
+?>
+        </div>
+    </div>
+</div>
+<?PHP
 
 get_footer();
