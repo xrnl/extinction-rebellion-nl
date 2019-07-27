6b91038e49a3fd9f9889325736bfea2c6a7a4eaf
Madhusudhan Srinivasa <me@madhums.me>
2019-07-27 14:21:14 +0200

    provide search in 404 page

--- web/app/themes/xrnl/404.php
+++ web/app/themes/xrnl/404.php
@@ -7,7 +7,8 @@ get_header(); ?>
 <div class="container">
     <div class="row mt-5">
         <div class="col-12">
-            <h1 class="my-5 py-5"><?php _e( 'Oops sorry! We were not able to find the page you were looking for...', 'xrnl' ); ?></h1>
+            <h1 class="pt-5"><?php _e( 'Oops sorry! We were not able to find the page you were looking for...', 'xrnl' ); ?></h1>
+            <?php get_search_form(); ?>
         </div>
     </div>
 </div>
