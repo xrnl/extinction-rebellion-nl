4a72afcb412330e46bf7d8bb93772cf03d3c0b3f
Madhusudhan Srinivasa <me@madhums.me>
2019-07-31 13:07:51 +0200

    add search form, other small layout enhancements

--- web/app/themes/xrnl/404.php
+++ web/app/themes/xrnl/404.php
@@ -8,7 +8,9 @@ get_header(); ?>
     <div class="row mt-5">
         <div class="col-12">
             <h1 class="pt-5"><?php _e( 'Oops sorry! We were not able to find the page you were looking for...', 'theme-xrnl' ); ?></h1>
-            <?php get_search_form(); ?>
+            <div class="my-5">
+                <?php get_search_form(); ?>
+            </div>
         </div>
     </div>
 </div>
--- web/app/themes/xrnl/template-parts/content-none.php
+++ web/app/themes/xrnl/template-parts/content-none.php
@@ -5,21 +5,18 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'theme-xrnl' ); ?></h1>
-	</header>
-
-	<div class="page-content">
-		<?php if ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'theme-xrnl' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php else : ?>
-
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'theme-xrnl' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php endif; ?>
-	</div>
+	<h1 class="page-title"><?php _e( 'Nothing Found', 'theme-xrnl' ); ?></h1>
+	<div class="mb-5">
+        <?php if ( is_search() ) : ?>
+            <p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'theme-xrnl' ); ?></p>
+            <div class="my-3">
+                <?php get_search_form(); ?>
+            </div>
+        <?php else : ?>
+            <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'theme-xrnl' ); ?></p>
+            <div class="my-3">
+                <?php get_search_form(); ?>
+            </div>
+        <?php endif; ?>
+    </div>
 </section>
