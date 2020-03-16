ad6cc47bcd2d9bdff33793753d5677bf3896726f
Madhusudhan Srinivasa <me@madhums.me>
2020-03-16 13:51:48 +0100

    changes to template based on cancellation

--- web/app/themes/xrnl/rwb.php
+++ web/app/themes/xrnl/rwb.php
@@ -9,20 +9,41 @@ get_header(); ?>
   <div class="bg-blue px-3 py-lg-5 pb-5 text-center text-white rwb-cover-image" style="background: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.45)), url('<?php the_field('rwb_cover_image_url'); ?>') no-repeat;">
     <div class="py-5">
       <h1 class="display-2"><?php the_title(); ?></h1>
-      <h1 class="font-xr text-uppercase"><?php the_field('rwb_caption'); ?></h1>
+      <?php if(get_field('rwb_caption')): ?>
+        <h1 class="font-xr text-uppercase">
+          <?php the_field('rwb_caption'); ?>
+        </h1>
+      <?php endif; ?>
     </div>
 
-    <h1 class="font-xr text-uppercase"><?php the_field('rwb_date'); ?></h1>
-    <div class="rwb-location">
-      <?php the_field('rwb_location'); ?>
-    </div>
+    <?php if(get_field('rwb_date')): ?>
+      <h1 class="font-xr text-uppercase"><?php the_field('rwb_date'); ?></h1>
+    <?php endif; ?>
+    <?php if(get_field('rwb_location')): ?>
+      <div class="rwb-location">
+        <?php the_field('rwb_location'); ?>
+      </div>
+    <?php endif; ?>
 
-    <div class="my-3">
-      <a class="btn btn-lg btn-pink" href="#join">
-        <?php _e('JOIN THE REBELLION', 'theme-xrnl'); ?></a>
-    </div>
+    <?php if(get_field('rwb_cta_title')): ?>
+      <div class="my-3">
+        <a class="btn btn-lg btn-pink" href="#join">
+          <?php the_field('rwb_cta_title'); ?>
+        </a>
+      </div>
+    <?php endif; ?>
 
-    <a href="<?php the_field('rwb_facebook_event_url'); ?>" target="_blank" class="text-white text-reset text-underline">Facebook</a> <?php _e('or', 'theme-xrnl') ?> <a class="text-white text-reset text-underline" href="<?php the_field('rwb_meetup_event_url'); ?>" target="_blank">Meetup</a>
+    <?php if(get_field('rwb_facebook_event_url')): ?>
+      <a href="<?php the_field('rwb_facebook_event_url'); ?>" target="_blank" class="text-white text-reset text-underline">Facebook</a>
+      &nbsp;
+    <?php endif; ?>
+    <?php if(get_field('rwb_facebook_event_url') && get_field('rwb_meetup_event_url')): ?>
+      <?php _e('or', 'theme-xrnl') ?>
+    <?php endif; ?>
+    <?php if(get_field('rwb_meetup_event_url')): ?>
+      &nbsp;
+      <a class="text-white text-reset text-underline" href="<?php the_field('rwb_meetup_event_url'); ?>" target="_blank">Meetup</a>
+    <?php endif; ?>
   </div>
 
   <div class="text-center my-sm-5 my-4">
@@ -37,12 +58,13 @@ get_header(); ?>
     </div>
   </div>
 
+  <?php if(get_field('rwb_join_title') && get_field('rwb_join_description') && get_field('sign_up_form_code')): ?>
   <div class="bg-pink py-sm-5 py-4">
     <a name="join"></a>
     <div class="container">
       <div class="row py-5 text-center">
         <div class="col-12 col-lg-8 mx-auto">
-          <h2><?php _e('JOIN', 'theme-xrnl'); ?></h2>
+          <h2><?php the_field('rwb_join_title'); ?></h2>
           <?php the_field('rwb_join_description'); ?>
         </div>
         <div class="col-12 col-lg-6 col-md-8 mx-auto mt-4">
@@ -51,27 +73,35 @@ get_header(); ?>
       </div>
     </div>
   </div>
+  <?php endif; ?>
 
+  <?php if(get_field('rwb_why_protest') && get_field('rwb_demands')): ?>
   <div class="container text-center py-sm-5 py-4">
     <div class="row py-5">
-      <div class="col-12 col-lg-8 mx-auto">
-        <h2>
-          <?php _e('WHY ARE WE REBELLING', 'theme-xrnl'); ?>?
-        </h2>
-        <p><?php the_field('rwb_why_protest'); ?></p>
-      </div>
-      <div class="col-12 col-lg-6 mx-auto mt-3">
-        <a class="btn btn-yellow btn-lg" data-toggle="collapse" href="#demands" role="button" aria-expanded="false" aria-controls="demands">
-          <?php _e('OUR DEMANDS', 'theme-xrnl'); ?>
-          <i class="fas fa-chevron-down"></i>
-        </a>
-        <div class="text-left collapse" id="demands">
-          <?php the_field('rwb_demands'); ?>
+      <?php if(get_field('rwb_why_protest')): ?>
+        <div class="col-12 col-lg-8 mx-auto">
+          <h2>
+            <?php _e('WHY ARE WE REBELLING', 'theme-xrnl'); ?>?
+          </h2>
+          <p><?php the_field('rwb_why_protest'); ?></p>
         </div>
-      </div>
+      <?php endif; ?>
+      <?php if(get_field('rwb_demands')): ?>
+        <div class="col-12 col-lg-6 mx-auto mt-3">
+          <a class="btn btn-yellow btn-lg" data-toggle="collapse" href="#demands" role="button" aria-expanded="false" aria-controls="demands">
+            <?php _e('OUR DEMANDS', 'theme-xrnl'); ?>
+            <i class="fas fa-chevron-down"></i>
+          </a>
+          <div class="text-left collapse" id="demands">
+            <?php the_field('rwb_demands'); ?>
+          </div>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
+  <?php endif; ?>
 
+  <?php if(get_field('rwb_who_is_xr')): ?>
   <div class="rwb-cover-image" style="background: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.55)), url('<?php the_field('rwb_why_protest_background_url'); ?>') no-repeat; min-height: 45vh;">
     <div class="container">
       <div class="row py-5 text-center text-white">
@@ -84,7 +114,9 @@ get_header(); ?>
       </div>
     </div>
   </div>
+  <?php endif; ?>
 
+  <?php if(have_rows('rwb_faqs')): ?>
   <div class="container-fluid my-sm-5 my-4">
     <a name="faq"></a>
     <div class="row py-5 text-center">
@@ -109,7 +141,9 @@ get_header(); ?>
       </div>
     </div>
   </div>
+  <?php endif; ?>
 
+  <?php if(have_rows('rwb_do_more')): ?>
   <div class="container-fluid text-center bg-blue py-sm-5 mt-5 py-4 mt-4">
     <div class="row pt-5">
       <div class="col-12 col-lg-8 mx-auto">
@@ -131,6 +165,7 @@ get_header(); ?>
       <?php } ?>
     </div>
   </div>
+  <?php endif; ?>
 </div>
 
 <?php get_footer(); ?>
