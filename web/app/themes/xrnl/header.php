7b28e7fe290a92598a5b704f30ede619224679f5
frnk <frxr@protonmail.com>
2020-07-09 23:09:11 +0200

    Switch from Simpleanalytics to Matomo

--- web/app/themes/xrnl/header.php
+++ web/app/themes/xrnl/header.php
@@ -67,10 +67,10 @@
                     <li class="mx-3 mx-lg-2">
                         <a href="https://www.instagram.com/extinctionrebellionnl/?hl=nl" target="_blank" class="insta" aria-label="instagram"><i class="fab text-black fa-instagram"></i></a></li>
                     <li class="mx-3 mx-lg-2">
-  <?php
-  $donatePage = icl_object_id(308,'page', true); // 308 is page ID
-  $donatePageURL = get_permalink( $donatePage );
-  ?>
+                        <?php
+                        $donatePage = icl_object_id(308,'page', true); // 308 is page ID
+                        $donatePageURL = get_permalink( $donatePage );
+                        ?>
                         <a href="<?php echo $donatePageURL ?>" class="btn btn-black"><?php _e('donate'); ?></a>
                     </li>
                 </ul>
