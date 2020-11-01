97d1702adfe417499f16bda808a834e77f45c177
franei <frank@yora.net>
2020-11-01 20:32:35 +0100

    Add author support for local group post type

--- web/app/themes/xrnl/functions.php
+++ web/app/themes/xrnl/functions.php
@@ -208,7 +208,7 @@ add_action('init', function(){
     $args = array(
         'label'                 => __( 'Local Group', 'text_domain' ),
         'labels'                => $labels,
-        'supports'              => array('title'),
+        'supports'              => array('title','author'),
         'hierarchical'          => false,
         'public'                => true,
         'show_ui'               => true,
