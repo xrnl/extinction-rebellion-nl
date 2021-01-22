8db8bf60e49d446a24fa8860476dd511dc9900ef
franei <frank@yora.net>
2021-01-22 22:09:42 +0100

    Switch from owl carousel to bootstrap

--- web/app/themes/xrnl/assets/js/xrnl-local-group-tabs.js
+++ web/app/themes/xrnl/assets/js/xrnl-local-group-tabs.js
@@ -7,31 +7,31 @@ const sections = ['about', 'demands', 'actions', 'events', 'positions', 'picture
 
 function checkSectionExists(section) {
   if (! jQuery('#' + section).length) {
-    jQuery('#lg-navigation').find('a[href=\'#' + section + '\']').parent().remove();
+    jQuery('#lg-navigation')
+    .find('a[href=\'#' + section + '\']')
+    .parent()
+    .remove();
   }
 }
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function($){
 
   // Remove optional sections from the navigation if they don't exist;
   // If there is only one nav-item left, remove that too.
   sections.forEach(checkSectionExists);
-  if (jQuery('#lg-navigation .nav .nav-item').length < 2){
-    jQuery('#lg-navigation .nav .nav-item').remove();
+  if ($('#lg-navigation .nav .nav-item').length < 2){
+    $('#lg-navigation .nav .nav-item').remove();
   };
 
-  // If the Over Ons section exists, make it active on page load (instead of Contact).
-  if (jQuery('#about-nav').length) {
-    jQuery('#about-nav').tab('show');
+  // If the About Us section exists, make it active on page load (instead of Contact).
+  if ($('#about-nav').length) {
+    $('#about-nav').tab('show');
   }
 
-  // Settings for the picture gallery
-  jQuery(".owl-carousel").owlCarousel({
-    loop:true,
-    margin:5,
-    nav:true,
-    items:1,
-    autoHeight:true
-  });
+  // Needed to initate the carousel
+  if ($('#pictures').length) {
+    $('.carousel-item:first').addClass('active');
+    $('.carousel-indicators > li:first').addClass('active');
+  };
 
 });
