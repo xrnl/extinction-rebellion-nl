5a715af25ee4b95391e40ea72ff61a83d62a9df0
charlotte-hib <charlotte.hibert@trifork.nl>
2021-02-17 20:34:20 +0100

    add transient for caching

--- web/app/themes/xrnl/affinity_group.php
+++ web/app/themes/xrnl/affinity_group.php
@@ -3,9 +3,6 @@
  * Template name: Affinity group
  */
 
-
-
-//TODO cache
 function query_all($endpoint)
 {
     $API_URL = 'https://actionnetwork.org/api/v2/';
@@ -34,11 +31,9 @@ function query_all($endpoint)
             $hasElement = false;
         }
     }
-
     return $responses;
 }
 
-//TODO cache
 function query($url)
 {
     $actionnetwork_api_key = get_field('actionnetwork_api_key', 'option');
@@ -53,45 +48,57 @@ function query($url)
 }
 
 
+function get_affinity_group()
+{
+    $res = get_transient('ags');
 
-// Hardcoded AN endpoints AG forms creation/update forms.
-$an_ag_endpoints = [
-    'forms/e8ac2f14-ba65-47fc-9560-90bd17f105fc/submissions'//,
-    // 'forms/d38377d0-be06-4853-ae82-06d0425e4918/submissions',
-    // 'forms/dac52069-261e-4485-8124-8e075dc84890/submissions',
-    // 'forms/8db3db83-38b1-4b9b-8251-6ef672f5cfaa/submissions'
+
+    // Hardcoded AN endpoints AG forms creation/update forms.
+    $an_ag_endpoints = [
+  'forms/e8ac2f14-ba65-47fc-9560-90bd17f105fc/submissions'//,
+  // 'forms/d38377d0-be06-4853-ae82-06d0425e4918/submissions',
+  // 'forms/dac52069-261e-4485-8124-8e075dc84890/submissions',
+  // 'forms/8db3db83-38b1-4b9b-8251-6ef672f5cfaa/submissions'
 ];
 
-$people_endpoints = array();
-foreach ($an_ag_endpoints as $endpoint) {
-    $all = query_all($endpoint);
-    foreach ($all as $response) {
-        // print_r($response);
-        $people = $response['_links']['osdi:person']['href'];
-        array_push($people_endpoints, $people);
-        // print_r($people_endpoints);
-    }
-}
-
-
-$ags = array();
-foreach ($people_endpoints as $url) { //TODO remove duplicated
-    $response = query($url);
-    $ag = array();
-
-    $fields = array("AG_name", "AG_size", "AG_n_non_arrestables", "AG_n_arrestables", "Municipality", "phone_number", "AG_regen_phone", "AG_comment");
-    $customFields = $response["custom_fields"];
-    foreach ($fields as $field) {
-        if (!array_key_exists($field, $customFields)) {
-            $customFields[$field] = "";
+    $people_endpoints = array();
+    foreach ($an_ag_endpoints as $endpoint) {
+        $all = query_all($endpoint);
+        foreach ($all as $response) {
+            // print_r($response);
+            $people = $response['_links']['osdi:person']['href'];
+            array_push($people_endpoints, $people);
+            // print_r($people_endpoints);
         }
-        $ag[$field] = $customFields[strval($field)];
     }
-    if (!array_key_exists("given_name", $response)) {
-        $response["given_name"] = "";
+
+
+    $ags = array();
+    foreach ($people_endpoints as $url) { //TODO remove duplicated
+        $response = query($url);
+        $ag = array();
+
+        $fields = array("AG_name", "AG_size", "AG_n_non_arrestables", "AG_n_arrestables", "Municipality", "phone_number", "AG_regen_phone", "AG_comment");
+        $customFields = $response["custom_fields"];
+        foreach ($fields as $field) {
+            if (!array_key_exists($field, $customFields)) {
+                $customFields[$field] = "";
+            }
+            $ag[$field] = $customFields[strval($field)];
+        }
+        if (!array_key_exists("given_name", $response)) {
+            $response["given_name"] = "";
+        }
+        $ag["given_name"] = $response["given_name"];
+        array_push($ags, $ag);
     }
-    $ag["given_name"] = $response["given_name"];
-    array_push($ags, $ag);
+    $res = json_encode($ags, JSON_PRETTY_PRINT);
+
+    set_transient('ags', $res, 10);
+
+    return $res;
 }
-$json_string = json_encode($ags, JSON_PRETTY_PRINT);
-print($json_string);
+
+echo(get_affinity_group());
+
+
